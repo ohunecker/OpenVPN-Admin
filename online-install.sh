fde5c71aa15bae9802dd0809c1d1d14d778c45a6
Oliver Hunecker <Oliver.Hunecker@polizei.slpol.de>
2020-10-12 17:50:35 +0200

    [fix] git-repo changed

--- online-install.sh
+++ online-install.sh
@@ -4,6 +4,6 @@ echo -e "${Green}Updating and Getting Ready"
 sudo apt update
 sudo apt install -y git
 cd ~
-git clone https://github.com/arvage/OpenVPN-Admin openvpn-admin
+git clone https://github.com/ohunecker/OpenVPN-Admin.git openvpn-admin
 cd openvpn-admin
 sudo ./install.sh /var/www www-data www-data
